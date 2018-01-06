84f53d7e0fe866eec6194a839f556f65e69f5605
alejandrox1 <alarcj137@gmail.com>
2018-01-06 08:47:14 -0500

    fix code blocks in stdin/out read

--- os/adv_programming_unix_env/ch01/02cpstdin_2stdout/main.c
+++ os/adv_programming_unix_env/ch01/02cpstdin_2stdout/main.c
@@ -10,13 +10,12 @@ int main() {
 	int n;
 	char buf[BUFFSIZE];
 
-	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) {
+	while ((n = read(STDIN_FILENO, buf, BUFFSIZE)) > 0) 
 		if (write(STDOUT_FILENO, buf, n) != n) 
 			err_sys("write error");
 
-		if (n < 0)
-			err_sys("read error");
+	if (n < 0)
+		err_sys("read error");
 
-		exit(0);
-	}
+	exit(0);
 }
