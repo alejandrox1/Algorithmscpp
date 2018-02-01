c8b6bdb2d213ea5f673675785a89993f4e4fbc07
alejandrox1 <alarcj137@gmail.com>
2018-02-01 11:08:53 -0500

    update on test client script

--- servers/filetransfer/testclient.sh
+++ servers/filetransfer/testclient.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+EXEC=fclient
+CMPDIR=../testserver
+
+# Run client
+./${EXEC}
+
+# check transfered files
 ls -l
 echo
 ls -lh
 
+# Compare transfered files with the originals
 for i in $(ls *.out); 
 do
-	diff <(xxd ${i}) <(xxd ../testserver/${i})
+	diff <(xxd ${i}) <(xxd ${CMPDIR}/${i})
 done
 
