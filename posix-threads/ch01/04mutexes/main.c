0fc28b39654d023d0985f6b17c43d2079dcae202
alejandrox1 <alarcj137@gmail.com>
2018-02-06 23:03:16 -0500

    replace tabs w/ spaces for alarm server

--- posix-threads/ch01/04mutexes/main.c
+++ posix-threads/ch01/04mutexes/main.c
@@ -6,9 +6,9 @@
 
 typedef struct alarm_tag {
 	struct alarm_tag	*link;
-	int 				seconds;
-	time_t 				time;
-	char				message[64];
+	int                 seconds;
+	time_t              time;
+	char                message[64];
 } alarm_t;
 
 pthread_mutex_t alarm_mutex = PTHREAD_MUTEX_INITIALIZER;
