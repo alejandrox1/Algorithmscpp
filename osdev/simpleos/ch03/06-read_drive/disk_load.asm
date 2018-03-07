2e4d7cc0442fc348e2629ae0b2bfadc6d1dcd271
alejandrox1 <alarcj137@gmail.com>
2018-03-07 14:30:29 -0500

    changes:osdev/simpleos/ch04/06/disk_load.asm improved the comments for disk_load method

--- osdev/simpleos/ch03/06-read_drive/disk_load.asm
+++ osdev/simpleos/ch03/06-read_drive/disk_load.asm
@@ -6,17 +6,30 @@
 ; 	dh number of sectors to be read.
 ; 	dl drive to read from.
 ;
+; Registers used
+;
+; ah will hold the read opcode
+; al needs the number of sectors to read from disk.
+;
+; ch select cylinder.
+; cl sector to begin reading from.
+; dh select a head/track (up or down, 0 | 1).
+; dl drive to read from.
+;
 disk_load:
  push dx         ; Store dx to later we can recall how many sectors we 
                  ; requested to read.
  mov ah, 0x02    ; BIOS read sector function.
- mov al, dh      ; Read dh sectors.
+ mov al, dh      ; Number of sectors to read.
+
  mov ch, 0x00    ; Select cylinder 0.
- mov dh, 0x00    ; Select head 0.
- mov cl, 0x02    ; Start reading from second sector (i.e., after boot sector).
+ mov cl, 0x02    ; Start reading from second sector (i.e., after boot sector).  
+                 ; 0x01 is the boot sector, 0x02 would then be free.            
+ mov dh, 0x00    ; Select head 0 (0 | 1 for up or down).
+ 
  int 0x13        ; BIOS interrupt
 
- jc disk_error1   
+ jc disk_error1  ; check if carry flag (cf) register was set. 
 
  pop dx
  cmp dh, al      ; al (secotrs read) cmp dh (sectors requested)
