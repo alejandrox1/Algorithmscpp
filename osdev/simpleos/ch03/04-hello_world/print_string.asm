bae3b898f4bd97cbc76878b9b306cc11b78d994f
alejandrox1 <alarcj137@gmail.com>
2018-03-07 11:38:07 -0500

    changes:osdev/simpleos/ch03/04/print_string.asm added extra comments

--- osdev/simpleos/ch03/04-hello_world/print_string.asm
+++ osdev/simpleos/ch03/04-hello_world/print_string.asm
@@ -12,10 +12,11 @@ print_string:
 
  loop_begin:
  	mov al, [bx]
- 	cmp al, 0
+ 	cmp al, 0       ; Strings end with a null byte.
  	je loop_end
- 	int 0x10
- 	add bx, 1
+
+ 	int 0x10        ; Print char.
+ 	add bx, 1       ; Move onto next byte.
  	jmp loop_begin
 
  loop_end: 
